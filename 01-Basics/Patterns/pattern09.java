08090a59949ee01ef47be36b01bac3a31b8656c4
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-06 20:36:31 +0530

    Pattern 09 | Diamond

--- 01-Basics/Patterns/pattern09.java
+++ 01-Basics/Patterns/pattern09.java
@@ -1,9 +1,9 @@
 public class pattern09 {
     public static void main(String[] args) {
-        int N = 5;
+        int N = 10;
 
         for (int row = 0; row < N; row++) {
-            
+
             for (int space = 0; space < N - row - 1; space++) {
                 System.out.print(" ");
             }
@@ -12,18 +12,20 @@ public class pattern09 {
                 System.out.print("*");
             }
 
-            for (int space = 0; space < N - row - 1; space++) {
-                System.out.print(" ");
-            }
-
             System.out.println();
         }
 
-        for(int row = 0; row < N; row++){
-            
-            for(int space = 0; space <= row; space++){
+        for (int row = 1; row < N; row++) {
+
+            for (int space = 0; space < row; space++) {
                 System.out.print(" ");
             }
+
+            for (int col = 0; col < 2 * N - (2 * row + 1); col++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
         }
     }
 }
