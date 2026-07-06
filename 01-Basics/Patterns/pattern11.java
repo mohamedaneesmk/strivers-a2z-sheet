8a760f0357153967b7f8237dfa6e7c4ec7db18ef
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-06 21:06:32 +0530

    Pattern 11

--- 01-Basics/Patterns/pattern11.java
+++ 01-Basics/Patterns/pattern11.java
@@ -1,11 +1,21 @@
 public class pattern11 {
     public static void main(String[] args) {
         int N = 5;
+        int start = 1;
 
         for (int row = 0; row < N; row++) {
-            for (int col = 0; col <= row; col++) {
-                System.out.print("1");
+
+            if (row % 2 == 0) {
+                start = 1;
+            } else {
+                start = 0;
             }
+
+            for (int col = 0; col <= row; col++) {
+                System.out.print(start);
+                start = 1 - start;
+            }
+
             System.out.println();
         }
     }
