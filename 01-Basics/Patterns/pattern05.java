46951e1ade6d2963faa681e6f06766a4ed2699c5
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-06 12:10:06 +0530

    Pattern 05

---
+++ 01-Basics/Patterns/pattern05.java
@@ -0,0 +1,12 @@
+public class pattern05 {
+    public static void main(String[] args) {
+        int N = 5;
+
+        for (int row = N; row >= 0; row--) {
+            for (int col = 0; col < row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
