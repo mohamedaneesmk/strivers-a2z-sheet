cb16595f667e43d465b9506e6c0956bcaadd3868
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-06 17:54:19 +0530

    Pattern 07 | Pyramid

---
+++ 01-Basics/Patterns/pattern07.java
@@ -0,0 +1,22 @@
+public class pattern07 {
+    public static void main(String[] args) {
+        int N = 10;
+
+        for (int row = 0; row < N; row++) {
+
+            for (int space = 0; space < N - row - 1; space++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 0; col < 2 * row + 1; col++) {
+                System.out.print("*");
+            }
+
+            for (int space = 0; space < N - row - 1; space++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
