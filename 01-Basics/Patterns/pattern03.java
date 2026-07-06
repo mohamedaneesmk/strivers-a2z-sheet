fa103bda7d25600f10d00cc79fdbd22f1b7a910e
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-06 12:03:43 +0530

    Pattern 02

--- 01-Basics/Patterns/pattern03.java
+++ 01-Basics/Patterns/pattern03.java
@@ -1,10 +1,10 @@
-public class pattern02 {
+public class pattern03 {
     public static void main(String[] args) {
         int N = 5;
 
         for (int row = 0; row < N; row++) {
             for (int col = 0; col <= row; col++) {
-                System.out.print("*");
+                System.out.print(col);
             }
             System.out.println();
         }
