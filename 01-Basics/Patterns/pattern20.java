7e4f93c9df3025228b2f1bc15d185b0c416c6b97
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-07 17:49:03 +0530

    Pattern 20

---
+++ 01-Basics/Patterns/pattern20.java
@@ -0,0 +1,42 @@
+public class pattern20 {
+    public static void main(String[] args) {
+
+        int N = 5;
+
+        // Upper half
+        for (int row = 0; row < N; row++) {
+
+            // Left stars
+            for (int col = 0; col <= row; col++)
+                System.out.print("*");
+
+            // Spaces
+            for (int space = 0; space < 2 * (N - row - 1); space++)
+                System.out.print(" ");
+
+            // Right stars
+            for (int col = 0; col <= row; col++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int row = N - 2; row >= 0; row--) {
+
+            // Left stars
+            for (int col = 0; col <= row; col++)
+                System.out.print("*");
+
+            // Spaces
+            for (int space = 0; space < 2 * (N - row - 1); space++)
+                System.out.print(" ");
+
+            // Right stars
+            for (int col = 0; col <= row; col++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
