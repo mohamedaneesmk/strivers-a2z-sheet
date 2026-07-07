a3ee8b410917024b4382ece06131e96b30d15c4f
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-07 11:26:51 +0530

    Pattern 13

---
+++ 01-Basics/Patterns/pattern13.java
@@ -0,0 +1,14 @@
+public class pattern13 {
+    public static void main(String[] args) {
+        int N = 5;
+        int count = 1;
+
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col <= row; col++) {
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
