7f96e575594c0b9f659ae5c0ac32584ede4d7adc
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-07 21:15:25 +0530

    Pattern 22

---
+++ 01-Basics/Patterns/pattern22.java
@@ -0,0 +1,17 @@
+public class pattern22 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int row = 0; row < 2 * n - 1; row++) {
+            for (int col = 0; col < 2 * n - 1; col++) {
+                int top = row;
+                int left = col;
+                int right = (2 * n - 2) - col;
+                int bottom = (2 * n - 2) - row;
+
+                System.out.print(n - Math.min(Math.min(top, bottom), Math.min(left, right)) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
