39337a5c5c2a5f5cc8ff894fdebd04c44bfd94fa
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-07 11:51:31 +0530

    Pattern 16

---
+++ 01-Basics/Patterns/pattern16.java
@@ -0,0 +1,13 @@
+public class pattern16 {
+    public static void main(String[] args) {
+        int N = 5;
+
+        for (int row = 0; row < N; row++) {
+            char ch = (char) ('A' + row);
+            for (int col = 0; col <= row; col++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
