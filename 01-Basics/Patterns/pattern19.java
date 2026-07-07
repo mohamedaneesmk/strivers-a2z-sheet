dc90dc46a51a4240b1d994b50d27a97cf9a65457
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-07 17:26:07 +0530

    Pattern 19

---
+++ 01-Basics/Patterns/pattern19.java
@@ -0,0 +1,41 @@
+public class pattern19 {
+    public static void main(String[] args) {
+        int N = 5;
+
+        // Upper half
+        for (int row = 0; row < N; row++) {
+
+            for (int col = 0; col <= N - row - 1; col++) {
+                System.out.print("*");
+            }
+
+            for (int space = 0; space < 2 * row; space++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 0; col <= N - row - 1; col++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int row = 1; row <= N; row++) {
+
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+
+            for (int space = 0; space < 2 * N - (2 * row); space++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
