af79f525f43470e435c75d835dac88f2f63778d1
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-07 16:23:56 +0530

    Pattern 18

---
+++ 01-Basics/Patterns/pattern18.java
@@ -0,0 +1,12 @@
+public class pattern18 {
+    public static void main(String[] args) {
+        int N = 5;
+
+        for (int row = 0; row < N; row++) {
+            for (char ch = (char) ('E' - row); ch <= 'E'; ch++) {
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+}
