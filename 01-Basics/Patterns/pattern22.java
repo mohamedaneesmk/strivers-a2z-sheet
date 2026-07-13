35ffec913bd4a979675172001d81198a1088e6a1
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-13 22:58:42 +0530

    Pattern 22 | Concentric Number

--- 01-Basics/Patterns/pattern22.java
+++ 01-Basics/Patterns/pattern22.java
@@ -3,16 +3,18 @@ public class pattern22 {
         int n = 5;
 
         for (int row = 0; row < 2 * n - 1; row++) {
+
             for (int col = 0; col < 2 * n - 1; col++) {
+
                 int top = row;
                 int left = col;
                 int right = (2 * n - 2) - col;
                 int bottom = (2 * n - 2) - row;
- 
+
                 System.out.print(n - Math.min(Math.min(top, bottom), Math.min(left, right)) + " ");
             }
+
             System.out.println();
         }
     }
 }
- 
