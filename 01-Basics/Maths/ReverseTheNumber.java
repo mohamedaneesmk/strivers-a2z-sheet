16378f2594f44d1786e5576670810aea75b1d521
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-13 11:46:58 +0530

    Reverse The Number

---
+++ 01-Basics/Maths/ReverseTheNumber.java
@@ -0,0 +1,21 @@
+package Maths;
+
+public class ReverseTheNumber {
+    public static void main(String[] args) {
+        int num = 123;
+
+        System.out.println(reverse(num));
+    }
+
+    private static int reverse(int num) {
+        int result = 0;
+
+        while (num != 0) {
+            int lastDigit = num % 10;
+            result = (result * 10) + lastDigit;
+            num = num / 10;
+        }
+
+        return result;
+    }
+}
