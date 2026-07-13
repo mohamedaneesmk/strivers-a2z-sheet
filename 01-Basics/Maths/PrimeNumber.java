9595db0b4889309ef9eebe2466a0267c998195b9
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-13 12:42:43 +0530

    Prime Number

--- 01-Basics/Maths/PrimeNumber.java
+++ 01-Basics/Maths/PrimeNumber.java
@@ -8,17 +8,17 @@ public class PrimeNumber {
     }
 
     private static boolean checkPrime(int num) {
-        int count = 0;
 
-        for (int i = 1; i * i <= num; i++) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
-                count++;
-                if (i != num / i) {
-                    count++;
-                }
+                return false;
             }
         }
 
-        return count == 2;
+        return true;
     }
 }
