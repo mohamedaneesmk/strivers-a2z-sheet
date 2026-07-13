493535afd7a0fd9f9b5e70d0200001eb06e5b37f
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-13 11:31:40 +0530

    Count the digits

---
+++ 01-Basics/Maths/CountDigits.java
@@ -0,0 +1,20 @@
+package Maths;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int num = 234;
+
+        System.out.println(findCount(num));
+    }
+
+    private static int findCount(int num) {
+        int count = 0;
+
+        while (num != 0) {
+            count++;
+            num = num / 10;
+        }
+
+        return count;
+    }
+}
