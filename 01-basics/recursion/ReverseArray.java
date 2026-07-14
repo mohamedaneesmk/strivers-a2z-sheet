7b11a4bf3e5c02963957ce8da8e8d6547b49adf4
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-14 22:15:34 +0530

    Reverse Array

---
+++ 01-basics/recursion/ReverseArray.java
@@ -0,0 +1,26 @@
+package recursion;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        reverse(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        reverse(arr, left + 1, right - 1);
+    }
+}
