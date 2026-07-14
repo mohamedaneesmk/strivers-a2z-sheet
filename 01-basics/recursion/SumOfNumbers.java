d63391d94d10ecf7c73ccfc6438113d9e2943d3a
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-14 18:01:43 +0530

    Sum of Numbers

--- 01-basics/recursion/SumOfNumbers.java
+++ 01-basics/recursion/SumOfNumbers.java
@@ -1,13 +1,14 @@
 package recursion;
-public class SumOfNumbers {
 
+public class SumOfNumbers {
     public static void main(String[] args) {
         int N = 4;
-        System.out.print(sumofNumbers(N));
+        System.out.println(sumofNumbers(N));
     }
 
     private static int sumofNumbers(int n) {
-        if(n == 0) return 0;
-        return n + sumofNumbers(n - 1); 
+        if (n == 0)
+            return 0;
+        return n + sumofNumbers(n - 1);
     }
 }
