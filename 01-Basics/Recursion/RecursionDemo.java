f687ff72e7fe5e8a4db10ea19616d457268d3b65
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-14 11:10:15 +0530

    Recursion basics

---
+++ 01-Basics/Recursion/RecursionDemo.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class RecursionDemo {
+    static int count = 0;
+
+    public static void main(String[] args) {
+        printName();
+        print();
+    }
+
+    private static void printName() {
+        if (count == 5)
+            return;
+        System.out.println("Anees");
+        count++;
+        printName();
+    }
+
+    private static void print() {
+        if (count == 5)
+            return;
+        System.out.println(count);
+        count++;
+        print();
+    }
+}
