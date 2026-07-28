7403d081d8fe756feea5ffb2418ea56788ec481e
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-07-28 18:23:35 +0530

    Insertion Sort

--- 02-sorting/InsertionSort.java
+++ 02-sorting/InsertionSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] nums = {64, 34, 25, 12, 22, 11, 90};
+        int[] nums = { 64, 34, 25, 12, 22, 11, 90 };
         insertionSort(nums);
 
         System.out.println(Arrays.toString(nums));
@@ -12,16 +12,15 @@ public class InsertionSort {
         int n = nums.length;
 
         for (int i = 1; i < n; i++) {
+
             int key = nums[i];
             int j = i - 1;
 
-            // Shift elements greater than key
             while (j >= 0 && nums[j] > key) {
                 nums[j + 1] = nums[j];
                 j--;
             }
 
-            // Insert key at the correct position
             nums[j + 1] = key;
         }
     }
