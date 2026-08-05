cdad692a1c4acf0671b86cd5c9b45266d25e88f1
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-08-05 13:39:58 +0530

    Set Matrix Zeros | LeetCode #73

--- 03-arrays/SetMatrix.java
+++ 03-arrays/SetMatrix.java
@@ -1,14 +1,63 @@
 public class SetMatrix {
     public static void main(String[] args) {
         int[][] matrix = {
-                { 1, 1, 1 },
-                { 1, 0, 1 },
-                { 1, 1, 1 }
+                { 0, 1, 2, 0 },
+                { 3, 4, 5, 2 },
+                { 1, 3, 1, 5 }
         };
 
         setZeroes(matrix);
 
         printMatrix(matrix);
+    }
+
+    // OPTIMAL SOLUTION
+    private static void setZeroes(int[][] matrix) {
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        for (int col = 0; col < matrix[0].length; col++) {
+            if (matrix[0][col] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            if (matrix[row][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        for (int row = 1; row < matrix.length; row++) {
+            for (int col = 1; col < matrix[0].length; col++) {
+                if (matrix[row][col] == 0) {
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
+                }
+            }
+        }
+
+        for (int row = 1; row < matrix.length; row++) {
+            for (int col = 1; col < matrix[0].length; col++) {
+                if (matrix[row][0] == 0 || matrix[0][col] == 0) {
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+
+        if (firstRowZero) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                matrix[0][col] = 0;
+            }
+        }
+
+        if (firstColZero) {
+            for (int row = 0; row < matrix.length; row++) {
+                matrix[row][0] = 0;
+            }
+        }
     }
 
     private static void printMatrix(int[][] matrix) {
@@ -20,25 +69,28 @@ public class SetMatrix {
         }
     }
 
-    private static void setZeroes(int[][] matrix) {
-        int[] row = new int[matrix.length];
-        int[] col = new int[matrix[0].length];
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
-                }
-            }
-        }
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (row[i] == 1 || col[j] == 1) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-    }
+    /*
+     * BETTER SOLUTION
+     * private static void setZeroes(int[][] matrix) {
+     * int[] row = new int[matrix.length];
+     * int[] col = new int[matrix[0].length];
+     * 
+     * for (int i = 0; i < matrix.length; i++) {
+     * for (int j = 0; j < matrix[0].length; j++) {
+     * if (matrix[i][j] == 0) {
+     * row[i] = 1;
+     * col[j] = 1;
+     * }
+     * }
+     * }
+     * 
+     * for (int i = 0; i < matrix.length; i++) {
+     * for (int j = 0; j < matrix[0].length; j++) {
+     * if (row[i] == 1 || col[j] == 1) {
+     * matrix[i][j] = 0;
+     * }
+     * }
+     * }
+     * }
+     */
 }
