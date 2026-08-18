da9d76edc0b5ebb93d35cb527a8822f593f9ce6a
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-08-18 12:05:06 +0530

    Binary Search | LeetCode #704

--- 04-binary-search/BinarySearch.java
+++ 04-binary-search/BinarySearch.java
@@ -7,11 +7,18 @@ public class BinarySearch {
     }
 
     private static int search(int[] nums, int target) {
-        int n = nums.length;
+        int left = 0;
+        int right = nums.length - 1;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == target) {
-                return i;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
