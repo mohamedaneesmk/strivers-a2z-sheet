c4b59940754c9200c08ac7fee6d0bf2c6c3632d7
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-08-25 20:18:02 +0530

    First And Last Position in Array | LeetCode #34

--- 04-binary-search/FirstAndLastPosition.java
+++ 04-binary-search/FirstAndLastPosition.java
@@ -9,59 +9,48 @@ public class FirstAndLastPosition {
     }
 
     private static int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int lowerBound = findLowerBound(nums, n, target);
 
-        int first = findFirst(nums, target);
-
-        if (first == -1) {
+        if (lowerBound == n || nums[lowerBound] != target) {
             return new int[] { -1, -1 };
         }
 
-        int last = findLast(nums, target);
-
-        return new int[] { first, last };
+        return new int[] { lowerBound, findUpperBound(nums, n, target) - 1 };
     }
 
-    private static int findLast(int[] nums, int target) {
-
-        int ans = -1;
-        int low = 0;
-        int high = nums.length - 1;
+    private static int findLowerBound(int[] nums, int n, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = n;
 
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target) {
                 ans = mid;
-                low = mid + 1; // Search Right
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
         return ans;
     }
 
-    private static int findFirst(int[] nums, int target) {
-
-        int ans = -1;
-        int low = 0;
-        int high = nums.length - 1;
+    private static int findUpperBound(int[] nums, int n, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = n;
 
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > target) {
                 ans = mid;
-                high = mid - 1; // Search left
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
         return ans;
     }
+
 }
