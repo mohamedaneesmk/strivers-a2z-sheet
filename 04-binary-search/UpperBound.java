d22f37f1c526d3cbe9c6cb42f4df2edf9f536df5
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-08-25 18:27:18 +0530

    Upper Bound | GeeksForGeeks

--- 04-binary-search/UpperBound.java
+++ 04-binary-search/UpperBound.java
@@ -6,15 +6,13 @@ public class UpperBound {
         System.out.println(upperBound(nums, x));
     }
 
-    private static int upperBound(int[] nums, int x) {
+    private static int upperBound(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
         int ans = nums.length;
-        int low = 0;
-        int high = nums.length - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (nums[mid] > x) {
+            if (nums[mid] > target) {
                 ans = mid;
                 high = mid - 1;
             } else {
