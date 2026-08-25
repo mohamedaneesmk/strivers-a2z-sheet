9fa7d39e15c39e425997d24594d9b1cccac95fab
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-08-25 18:28:54 +0530

    Search Insert Position | LeetCode #35

--- 04-binary-search/SearchInsertPosition.java
+++ 04-binary-search/SearchInsertPosition.java
@@ -7,21 +7,19 @@ public class SearchInsertPosition {
     }
 
     private static int searchInsert(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
+            if (nums[mid] >= target) {
+                ans = mid;
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
-        return low;
+        return ans;
     }
 }
