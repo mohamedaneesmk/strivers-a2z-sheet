8b0f38b16f5e07a72f05ab9da3dcbd8988a95635
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-09-10 19:32:24 +0530

    Find Sqaure Root | LeetCode #69

--- 04-binary-search/SquareRoot.java
+++ 04-binary-search/SquareRoot.java
@@ -1,22 +1,31 @@
+
 public class SquareRoot {
+
     public static void main(String[] args) {
         int num = 28;
         System.out.println(findSqaureRoot(num));
     }
 
     private static int findSqaureRoot(int num) {
-        if (num < 2) return num;
-        
-        int low = 1, high = num / 2, ans = 1;
+        if (num == 0) {
+            return 0;
+        }
+
+        long low = 1, high = num;
+        long ans = 1;
+
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (mid <= num / mid) {
+            long mid = low + (high - low) / 2;
+            long sq = mid * mid;
+
+            if (sq <= num) {
                 ans = mid;
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return ans;
+
+        return (int) ans;
     }
 }
